36bdd7a70dd843328269b82f4e245a50e91ff530
amstelart <amsteldroid@gmail.com>
2020-12-19 19:05:54 +0300

    промежуточный 3

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,9 +1,119 @@
 // Если на проекте jQuery
 $(document).ready(function () {
   // code
-  // $(function() {
-  //   $('input, select').styler();
+  // star-rating
+  $(".product-rating").starRating({
+    readOnly: true,
+    starSize: 22,
+    useGradient: false,
+    starShape: 	'rounded',
+    activeColor: '#FFB800',
+    emptyColor: '#E9E3E1',
+  });
+  // $(".product-rating").starRating({
+  //   starSize: 25,
+  //   setReadOnly: true,
+  //   callback: function (currentRating, $el) {
+  //     // make a server call here
+  //   }
   // });
+  // range-slider
+  $(function () {
+
+    var $range = $(".js-range-slider"),
+      $inputFrom = $(".js-input-from"),
+      $inputTo = $(".js-input-to"),
+      instance,
+      min = 0,
+      max = 10000,
+      from = 0,
+      to = 0;
+
+    $range.ionRangeSlider({
+      type: "double",
+      skin: "round",
+      min: min,
+      max: max,
+      from: 0,
+      to: 10000,
+      hide_min_max: true,
+      hide_from_to: true,
+
+      onStart: updateInputs,
+      onChange: updateInputs,
+      step: 100,
+      prettify_enabled: true,
+      prettify_separator: ".",
+      values_separator: " - ",
+      force_edges: true
+    });
+
+    instance = $range.data("ionRangeSlider");
+
+    function updateInputs(data) {
+      from = data.from;
+      to = data.to;
+
+      $inputFrom.prop("value", from);
+      $inputTo.prop("value", to);
+    }
+
+    $inputFrom.on("input", function () {
+      var val = $(this).prop("value");
+
+      // validate
+      if (val < min) {
+        val = min;
+      } else if (val > to) {
+        val = to;
+      }
+
+      instance.update({
+        from: val
+      });
+    });
+
+    $inputTo.on("input", function () {
+      var val = $(this).prop("value");
+
+      // validate
+      if (val < from) {
+        val = from;
+      } else if (val > max) {
+        val = max;
+      }
+
+      instance.update({
+        to: val
+      });
+    });
+
+  });
+  // end range-slider
+  // $(function() {
+  //   $('.sort-item').styler();
+  // });
+
+  $('.product-carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    draggable: false,
+    asNavFor: '.product-carousel-nav'
+  });
+  $('.product-carousel-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.product-carousel',
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    draggable: false,
+    focusOnSelect: true
+  });
+
   $('.main-carousel').slick({
     dots: false,
     arrows: false,
